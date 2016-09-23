843f75b6aa5f029644aeb6d4d58601ae3c49e5b4
Konstantin <iwebcat@yandex.ru>
2016-09-23 21:15:38 +1000

    main features
    Added main features

---
+++ Assets/Main/Mechanics/CardMechanics.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMechanics : MonoBehaviour {
+    //Поля
+    int id, localId, x, y,cardClass, cardAttack, cardHp, cardSpeed, npSpeed, cardRarity;
+
+
+
+	// Создание
+	void Start () {
+	
+	}
+	
+	// обновление
+	void Update () {
+	
+	}
+
+    void Attack(int enemyId)
+    {
+
+    }
+
+    void Death()
+    {
+
+    }
+
+
+}
